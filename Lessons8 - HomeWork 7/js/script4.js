7ba1d34af8563e45882b6162c676b0f9d063ad23
SerhiiBS <100538411+SerhiiBS@users.noreply.github.com>
2022-05-30 16:07:17 +0300

    fixed script 4

--- Lessons8 - HomeWork 7/js/script4.js
+++ Lessons8 - HomeWork 7/js/script4.js
@@ -1,13 +1,11 @@
 const jsButton5 = document.querySelector(".js__button5");
-const imagesArray = ["1.jpg", "2.jpg", "3.jpg" ,"4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"];
-const imgOut = document.querySelector("js__img_out")
+const imgOut = document.querySelector(".js__img_out")
 function imgRandom() {
+    const random = Math.ceil(Math.random() * 10);
+    const img = document.createElement('img');
+    img.src = 'img/'+random+'.jpg';
+    imgOut.append(img);  
     
-    for (let i = 0; i < imagesArray.length; i++); {
-        let img = document.createElement('img');
-        img.src = "img/"+imagesArray[i]+".jpg";
-        
-    }
 }
 
 jsButton5.addEventListener("click", imgRandom);
