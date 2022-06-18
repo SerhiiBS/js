47697e0b0e6db4d596a1c740df2ad4bb4f625308
SerhiiBS <100538411+SerhiiBS@users.noreply.github.com>
2022-06-18 21:27:46 +0300

    Update script.js

--- Lessons11 - Homework 9/script.js
+++ Lessons11 - Homework 9/script.js
@@ -2,34 +2,43 @@ const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 const slides = document.querySelectorAll('.slide');
 
+
 let currentIndexSlide = 0;
+let timer;
 
-function showButtons() {
-    if (currentIndexSlide === 0) {
-        buttonPrev.disabled = true;
-    } else {
-        buttonPrev.disabled = false;
+const slider = function () {
+
+    function showButtons() {
+        buttonPrev.disabled = currentIndexSlide === 0;
+        buttonNext.disabled = currentIndexSlide === slides.length - 1;
     }
-    if (currentIndexSlide === slides.length - 1) {
-        buttonNext.disabled = true;
-    } else {
-        buttonNext.disabled = false;
+    showButtons();
+
+    const nextSlide = function() {
+            slides[currentIndexSlide].classList.remove('active');
+            currentIndexSlide++;
+            slides[currentIndexSlide].classList.add('active');
+            showButtons();
+            clearInterval(timer);
+            autoSlider();
     }
-}
-showButtons();
 
-const nextSlide = function() {
-    slides[currentIndexSlide].classList.remove('active');
-    currentIndexSlide++;
-    slides[currentIndexSlide].classList.add('active');
-    showButtons();
-}
-const prevSlide = function() {
-    slides[currentIndexSlide].classList.remove('active');
-    currentIndexSlide--;
-    slides[currentIndexSlide].classList.add('active');
-    showButtons();
-}
+    const prevSlide = function() {
+            slides[currentIndexSlide].classList.remove('active');
+            currentIndexSlide--;
+            slides[currentIndexSlide].classList.add('active');
+            showButtons();
+    }
 
-buttonNext.addEventListener('click', nextSlide);
-buttonPrev.addEventListener('click', prevSlide);
+    const autoSlider = function () {
+        timer = setTimeout(nextSlide, 5000);
+        if (currentIndexSlide === slides.length - 1) {
+            clearTimeout(timer);
+        }
+    }
+    autoSlider();
+
+    buttonNext.addEventListener('click', nextSlide);
+    buttonPrev.addEventListener('click', prevSlide);
+}
+    slider();
