d757e0b7f310d99b06336aa86cb7ba08e67d25f6
SerhiiBS <original.dn.ua@gmail.com>
2022-07-02 18:06:36 +0000

    add

---
+++ temp3/script.js
@@ -0,0 +1,11 @@
+$(function (){
+    console.log(slider.$slider.attr('first'))
+    // const slider = {
+    //     sliderTimerID: undefined,
+    //     $slider: $('p'),
+    //     nextSlide() {
+    //         let currentSlide = slider.$slider.attr('first');
+    //         console.log(currentSlide)
+    //     }
+    // }
+})
