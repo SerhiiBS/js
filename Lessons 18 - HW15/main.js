4967589060bea170272587be58ec4b2590d7a398
Serhii <original.dn.ua@gmail.com>
2022-07-03 22:27:50 +0300

    add hw15

---
+++ Lessons 18 - HW15/main.js
@@ -0,0 +1,9 @@
+
+$.ajax({
+    type: "GET",
+    url: "http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19",
+    success: (response) => {
+        console.log(response)
+        $('js--input').html(JSON.stringify(response))
+    }
+})
